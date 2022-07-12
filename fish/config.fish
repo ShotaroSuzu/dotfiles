6ae090129b318872ff827e108525b0740a74a7d0
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-07-12 14:19:15 +0900

    Add fish

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    set -g theme_date_format "+%Y-%m-%d %H:%M:%S"
+    set -g fish_prompt_pwd_dir_length 0
+    # Commands to run in interactive sessions can go here
+    
+end
